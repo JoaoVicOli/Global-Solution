57df57038a1c6c62a13d456d8fdb1b6c2217c6ce
JoaoVicOli <joaovictoroliveiradossantos18@gmail.com>
2024-06-04 12:09:31 -0300

    Arrumando as seções

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,19 @@ document.getElementById('irParaSecao').addEventListener('click', function() {
     const selecao = document.getElementById('secoesDropdown').value;
     window.location.href = selecao;
 });
+
+let imagens=["./assets/img/lixo1.png", "./assets/img/lixo2.png", "./assets/img/peixeMorto1.png", "./assets/img/peixeMorto2.png", "./assets/img/acidente.png" ];
+let index = 0;
+let time = 3000;
+
+function slideShow(){
+
+    document.getElementById("imgbanner").src=imagens[index];
+    index++;
+    
+        if(index == imagens.length){
+            index=0;
+        }
+        setTimeout("slideShow()", time);
+    }
+    slideShow();
