966edb81d3dddaa461a2afcd2ab794fa9f1776d3
JoaoVicOli <joaovictoroliveiradossantos18@gmail.com>
2024-06-06 15:06:51 -0300

    Responsividade

--- assets/python/python.py
+++ assets/python/python.py
@@ -12,14 +12,14 @@ while i < 2:
     i+=1
     ph = aleatorio(1, 15)                                    
     temperatura = aleatorio(0, 30)
-    if 7.8 <= ph or ph => 8.2:
+    if 7.8 <= ph or ph > 8.2:
         print(f"O ph está muito baixo: {ph}, resolver rápido")
         conferir_ph.append(ph)
         print(conferir_ph)
     else:
         print(f"O ph está aceitável: {ph}")
 
-    if temperatura <= 10 or temperatura => 30:
+    if temperatura <= 10 or temperatura > 30:
         print("A temperatura está com problema, resolver rápido")
         conferir_temperatura.append(temperatura)
         print(conferir_temperatura)
