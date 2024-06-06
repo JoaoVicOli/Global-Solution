dad2915337932fe937289da3bd4746a9ac89dd18
JoaoVicOli <joaovictoroliveiradossantos18@gmail.com>
2024-06-06 14:19:37 -0300

    Site finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const usuario = document.getElementById('usuario').value;
     const senha = document.getElementById('senha').value;
     
-    if (usuario === "charmander" && senha === "12345"){
+    if (usuario === "solution" && senha === "12345"){
         alert('Sucesso liberando seções');
         document.getElementById('dropdownContainer').style.display = 'flex'; 
         document.querySelector('.header-login').addEventListener('submit', function(){
--- assets/js/secao1.js
+++ assets/js/secao1.js
@@ -1,6 +1,6 @@
-let imagens1 = ["./assets/img/acidflask.png", "./assets/img/thermometer.png"];
+let imagens1 = ["./assets/img/acidflask.png", "./assets/img/thermometer.png", "./assets/img/pollution_7925306.png"];
 let index1 = 0;
-let time1 = 3000;
+let time1 = 2000;
 
 function slideShow1() {
     document.getElementById("imgbanner1").src = imagens1[index1];
